96b4253b740f9e2a61ad40894c4ae8c7d9127d1d
Okky Dwi Triferyawan <61794853+okky-chan@users.noreply.github.com>
2022-04-06 13:34:30 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+#include "TcpListener.h"
+
+using namespace std;
+
+void Listener_MessageReceived(CTcpListener* listener, int client, string msg);
+
+void main() {
+	CTcpListener server("127.0.0.1", 54000, Listener_MessageReceived);
+
+	if (server.Init()) {
+		server.Run();
+	}
+}
+
+void Listener_MessageReceived(CTcpListener* listener, int client, string msg) {
+	listener->Send(client, msg);
+}
